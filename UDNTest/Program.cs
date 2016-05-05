cca3506c8bab2f69d95593e209ec15cd52362fee
Serg-Norseman <serg.zhdanovskih@yandex.ru>
2016-05-06 00:39:55 +0500

    I changed my mind. So it will be better

--- UDNTest/Program.cs
+++ UDNTest/Program.cs
@@ -20,11 +20,11 @@ namespace UDNTest
             switch (calendar)
             {
                 case CalendarType.ctGregorian:
-                    this.Value = UDNHelper.getGregorianUDN(year, month, day);
+                    this.Value = UDNHelper.GetGregorianUDN(year, month, day);
                     break;
                     
                 case CalendarType.ctJulian:
-                    this.Value = UDNHelper.getJulianUDN(year, month, day);
+                    this.Value = UDNHelper.GetJulianUDN(year, month, day);
                     break;
             }
         }
@@ -45,17 +45,17 @@ namespace UDNTest
             fDates.Add(new UDN(CalendarType.ctJulian, 2016, 04, 21, "2016/05/04 [g] = 2016/04/21 [j]"));
             fDates.Add(new UDN(CalendarType.ctJulian, 2016, 04, 23, "2016/05/06 [g] = 2016/04/23 [j]"));
             
-            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, 05, UDNHelper.UNKNOWN_DAY, "2016/05/?? [g]")); // must be first
-            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, 06, UDNHelper.UNKNOWN_DAY, "2016/06/?? [g]")); // must be last
+            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, 05, UDNHelper.UnknownDay, "2016/05/?? [g]")); // must be first
+            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, 06, UDNHelper.UnknownDay, "2016/06/?? [g]")); // must be last
 
-            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UNKNOWN_YEAR, UDNHelper.UNKNOWN_MONTH, UDNHelper.UNKNOWN_DAY, "??/??/?? [g]"));
-            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UNKNOWN_YEAR, 04, 23, "??/04/23 [g]"));
-            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UNKNOWN_YEAR, 03, 23, "??/03/23 [g]"));
-            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UNKNOWN_YEAR, UDNHelper.UNKNOWN_MONTH, 23, "??/??/23 [g]"));
-            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, UDNHelper.UNKNOWN_MONTH, UDNHelper.UNKNOWN_DAY, "2016/??/?? [g]"));
-            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, UDNHelper.UNKNOWN_MONTH, 10, "2016/??/10 [g]"));
+            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UnknownYear, UDNHelper.UnknownMonth, UDNHelper.UnknownDay, "??/??/?? [g]"));
+            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UnknownYear, 04, 23, "??/04/23 [g]"));
+            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UnknownYear, 03, 23, "??/03/23 [g]"));
+            fDates.Add(new UDN(CalendarType.ctGregorian, UDNHelper.UnknownYear, UDNHelper.UnknownMonth, 23, "??/??/23 [g]"));
+            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, UDNHelper.UnknownMonth, UDNHelper.UnknownDay, "2016/??/?? [g]"));
+            fDates.Add(new UDN(CalendarType.ctGregorian, 2016, UDNHelper.UnknownMonth, 10, "2016/??/10 [g]"));
             fDates.Add(new UDN(CalendarType.ctGregorian, 2015, 03, 23, "2015/03/23 [g]"));
-            fDates.Add(new UDN(CalendarType.ctGregorian, 2014, UDNHelper.UNKNOWN_MONTH, 23, "2014/??/23 [g]"));
+            fDates.Add(new UDN(CalendarType.ctGregorian, 2014, UDNHelper.UnknownMonth, 23, "2014/??/23 [g]"));
             fDates.Add(new UDN(CalendarType.ctGregorian, 2016, 05, 31, "2016/05/31 [g]"));
             fDates.Add(new UDN(CalendarType.ctGregorian, 2016, 05, 31, "2016/05/31 [g]"));
             fDates.Add(new UDN(CalendarType.ctGregorian, -4712, 1, 2, "-4712/01/02 [g]"));
@@ -66,11 +66,11 @@ namespace UDNTest
 
             fDates.Add(new UDN(CalendarType.ctGregorian, 1, 1, 3, "0001/01/03 [g]"));
 
-            fDates.Sort(delegate(UDN left, UDN right) { return UDNHelper.compareUDN(left.Value, right.Value); });
+            fDates.Sort(delegate(UDN left, UDN right) { return UDNHelper.CompareUDN(left.Value, right.Value); });
             
             foreach (UDN udn in fDates)
             {
-                Object[] a = {udn.Value, (UDNHelper.VALUE_MASK & udn.Value), udn.Calendar.ToString(), udn.Description};
+                Object[] a = {udn.Value, (UDNHelper.ValueMask & udn.Value), udn.Calendar.ToString(), udn.Description};
                 Console.WriteLine("Value: {0, 12}\t(unmasked value: {1, 12})\t{2}\t{3}", a);
             }
             
