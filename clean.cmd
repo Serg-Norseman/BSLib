d0a167b4509e3ad2294533ee43abfd8db6481b79
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2016-06-02 00:47:20 +0300

    Minor cleaning

---
+++ clean.cmd
@@ -0,0 +1,13 @@
+rem @echo off
+
+cls
+
+rmdir .\CalendarConverterLib\bin /s /q
+rmdir .\CalendarConverterLib\obj /s /q
+
+rmdir .\CalendarConverterTests\bin /s /q
+rmdir .\CalendarConverterTests\obj /s /q
+
+rmdir .\UDNTest\bin /s /q
+rmdir .\UDNTest\obj /s /q
+
