9148b5566ab0171178132ae6ad86b0cb8e188ff5
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2018-06-30 01:15:14 +0300

    Added input validation

---
+++ BSLib.Controls/Validation/NumericFieldValidator.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *  "BSLib.Controls".
+ *  Copyright (C) 2017 by Sergey V. Zhdanovskih.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace BSLib.Controls.Validation
+{
+    public class NumericFieldValidator : ValidatorBase
+    {
+        internal const string NUMERIC_FIELD_REQUIRE = "Require numeric value";
+
+        public NumericFieldValidator()
+        {
+            ErrorMessage = NUMERIC_FIELD_REQUIRE;
+        }
+
+        protected override bool EvaluateIsValid()
+        {
+            string text = ControlToValidate.Text;
+
+            double value;
+            bool result = double.TryParse(text, out value);
+            return result;
+        }
+    }
+}
