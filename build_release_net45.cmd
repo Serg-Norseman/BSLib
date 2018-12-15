0d4c647e0042c50ab35a7009012233892f3a4bf2
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2018-12-15 22:39:23 +0300

    Update cmd scripts

--- build_release_net45.cmd
+++ build_release_net45.cmd
@@ -1,7 +1,11 @@
 @set PROGFILES=%PROGRAMFILES%
 @if exist "%PROGRAMFILES(x86)%" set PROGFILES=%PROGRAMFILES(x86)%
 
-"%PROGFILES%\MSBuild\12.0\Bin\msbuild" /m BSLib.sln /t:Rebuild /p:Configuration=Release "/p:Platform=Any CPU" /p:TargetFrameworkVersion=v4.5 %*
+set MSB="msbuild_not_found"
+@if exist "%PROGFILES%\MSBuild\12.0\Bin\msbuild" set MSB=%PROGFILES%\MSBuild\12.0\Bin\msbuild
+@if exist "%PROGFILES%\MSBuild\14.0\Bin\msbuild" set MSB=%PROGFILES%\MSBuild\14.0\Bin\msbuild
+
+"%MSB%" /m BSLib.sln /t:Rebuild /p:Configuration=Release "/p:Platform=Any CPU" /p:TargetFrameworkVersion=v4.5 %*
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
 
