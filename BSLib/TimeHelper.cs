0e9b9874397721885547360f3031b1a2fee2b980
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2022-03-14 01:04:57 +0300

    Added time functions

---
+++ BSLib/TimeHelper.cs
@@ -0,0 +1,52 @@
+﻿/*
+ *  "BSLib".
+ *  Copyright (C) 2009-2022 by Sergey V. Zhdanovskih.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace BSLib
+{
+    public static class TimeHelper
+    {
+        private static readonly DateTime UnixTimeStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>
+        /// Converts the given date value to Unix epoch time.
+        /// </summary>
+        public static long DateTimeToUnixTime(DateTime dateTime)
+        {
+            var date = dateTime.ToUniversalTime();
+            var ticks = date.Ticks - UnixTimeStart.Ticks;
+            var ts = ticks / TimeSpan.TicksPerSecond;
+            return ts;
+        }
+
+        /// <summary>
+        /// Converts the given Unix epoch time to a <see cref="DateTime"/> with <see cref="DateTimeKind.Utc"/> kind.
+        /// </summary>
+        public static DateTime UnixTimeToDateTime(long intDate)
+        {
+            var timeInTicks = intDate * TimeSpan.TicksPerSecond;
+            return UnixTimeStart.AddTicks(timeInTicks);
+        }
+
+        public static long GetUtcNow()
+        {
+            return DateTimeToUnixTime(DateTime.UtcNow);
+        }
+    }
+}
