90356f1ef860d20b7451671e0712bd0108092109
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2022-03-22 16:41:04 +0300

    Small update

--- BSLib.Design.WFHandlers/AssemblyInfo.cs
+++ BSLib.Design.WFHandlers/AssemblyInfo.cs
@@ -5,9 +5,9 @@
 [assembly: AssemblyTitle("BSLib.Design.WFHandlers")]
 [assembly: AssemblyDescription("Design Control's Handlers for WinForms")]
 [assembly: AssemblyProduct("BSLib")]
-[assembly: AssemblyCopyright("Copyright © 2019-2020 by Sergey V. Zhdanovskih")]
+[assembly: AssemblyCopyright("Copyright © 2019-2022 by Sergey V. Zhdanovskih")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 #endif
