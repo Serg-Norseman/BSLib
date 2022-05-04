a82ce0cdd72aa936b157decb3c9e46fee483ba7a
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2022-05-04 16:05:42 +0300

    Improved BSLib.Design

--- BSLib.Design/AssemblyInfo.cs
+++ BSLib.Design/AssemblyInfo.cs
@@ -5,9 +5,9 @@
 [assembly: AssemblyTitle("BSLib.Design")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("BSLib")]
-[assembly: AssemblyCopyright("Copyright © 2019-2020 by Sergey V. Zhdanovskih")]
+[assembly: AssemblyCopyright("Copyright © 2019-2022 by Sergey V. Zhdanovskih")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 #endif
