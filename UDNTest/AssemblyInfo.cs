0e468303d139bcee488485a45f19ee39e79c8ffc
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2023-01-16 01:25:55 +0300

    Refactoring and cleaning

--- UDNTest/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("UDNTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("BSLib")]
-[assembly: AssemblyCopyright("Copyright 2016 by Serg V. Zhdanovskih, Ruslan Garipov")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyCulture("")]
